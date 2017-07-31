0d60fea0ec6950139da9ad5f52dbde9fea24ff9a
JOYN28 <kimyoonseo@kaist.ac.kr>
2017-07-31 21:25:26 +0900

    Nothing

--- app/src/main/java/com/example/q/cs496_pj5_sandart/MainActivity.java
+++ app/src/main/java/com/example/q/cs496_pj5_sandart/MainActivity.java
@@ -3,9 +3,25 @@ package com.example.q.cs496_pj5_sandart;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+  // Top menu layout
+  LinearLayout topMenu;
+  // Top menu buttons
+  ImageView thick;
+  ImageView erase;
+  ImageView undo;
+  ImageView redo;
+  ImageView save;
+  ImageView trash;
+
+
+  private int seekBarThickProgress, seekBarEraseProcess;
+  private View popupLayout, popupEraseLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +35,16 @@ public class MainActivity extends AppCompatActivity {
     MyView myView = new MyView(this, getApplicationContext());
     layout.addView(myView, 0);
 
+    // Binding
+    erase = (ImageView) this.findViewById(R.id.erase);
+    topMenu = (LinearLayout) this.findViewById(R.id.topMenu);
+
   }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+  }
+
+
 }
